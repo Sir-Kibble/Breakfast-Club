bd0d44f06c08b9397d12dad14753c22f6686d7c8
Mole <Mole@10.0.1.21>
2013-08-25 20:55:53 -0400

    test

---
+++ src/breakfast/club/BreakfastClub.java
@@ -0,0 +1,12 @@
+package breakfast.club;
+
+/**
+ *
+ * @author Mole
+ */
+public class BreakfastClub {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

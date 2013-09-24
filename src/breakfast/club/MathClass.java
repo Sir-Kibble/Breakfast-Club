99dd6a9b7a7f0661f74f7c68ad05d4e543c5c088
Janel <Janel@10.10.234.135>
2013-09-24 13:44:40 -0400

    attempting to push math class?

--- src/breakfast/club/MathClass.java
+++ src/breakfast/club/MathClass.java
@@ -3,11 +3,13 @@ package breakfast.club;
  *
  * @author Mole
  */
+import java.util.*;
+
 public class MathClass {
     public static void main(String[] args) {
-        
-    }
+       //Math1 m = new Math1();
     
+    }    
     class Math1 {
         //x-axis
         double tableWidth;
@@ -15,6 +17,9 @@ public class MathClass {
         double lcl = 0; //lower class limit
         double ucl = 0; //upper class limit
         
+        public Math1() {
+            
+        }
         public Math1(double n, double tableWidth, double lcl, double ucl) {
             this.n = n;
             this.tableWidth = tableWidth;
@@ -39,6 +44,38 @@ public class MathClass {
             
             //x-axis total width?
         }
+        
+        public double[] barHeight( double numBars, double[] values ) {
+            
+            double sum = 0; //Sum of values 
+            double classNum = 0; //Number of classes: sum of values divided by number of bars
+            
+            int count = 1;
+            double[]  barDimen = new double[100];
+            double tempSum = values[0];
+            int barLocation = 0;
+          
+            for( int i = 0; i < values.length; i++ ) {
+                Arrays.sort( values );
+                sum = sum + values[i];
+            }
+            
+            classNum = sum / numBars; 
+            
+            // ???
+            for( int i = 1; i < numBars; i++ ) {
+                if( i == classNum * count ) {
+                   barDimen[ barLocation ] = tempSum / numBars;
+                   tempSum = 0;
+                   barLocation++;
+                   count++;
+                }
+                tempSum += values[i];
+            }
+            
+            return barDimen;
+        }
     }
 }
+
 
